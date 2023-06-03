5e90eb6fd8ae045ee90365fefaaaaab7dc819d65
mauro-balades <mauro.balades@tutanota.com>
2023-06-03 08:26:38 +0200

    Fixed module entry point and requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='shaggy',
-    version='1.0.2',
+    version='1.0.3',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Mauro Baladés',
     author_email='mauro.balades@tutanota.com',
     description='Discover and connect with social media accounts across multiple platforms using just a username with SocialTrackr, the ultimate account hunting software.',
     url='https://github.com/mauro-balades/shaggy',
-    packages=find_packages(),
+    packages=["shaggy"],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'shaggy = shaggy.__main__:main',
         ],
     },
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
